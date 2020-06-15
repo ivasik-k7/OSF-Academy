515ad02c2ca26c2917a50de44d896c3416923a40
ivasik-k7 <jeanlacroix@ukr.net>
2020-06-15 20:57:09 +0300

    hero .a

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
+    //BURGER//
     $(".connector").click(() => {
         $(".connector").toggleClass("isActiveBurger");
+    });
+    $(".hero-slider").slick({
+        arrows: false,
+        dots: true,
     });
 });
